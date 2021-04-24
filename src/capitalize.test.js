658cd1a1bc43f7683fa581ebb75460eeebf9754b
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-24 23:34:36 +0200

    Add last test for capitalize function

--- src/capitalize.test.js
+++ src/capitalize.test.js
@@ -3,3 +3,7 @@ import capitalize from './capitalize.js';
 test('first character is capitalized', () => {
   expect(capitalize('banana')).toMatch(/^[A-Z]/);
 });
+
+test("other characters don't change", () => {
+  expect(capitalize('iNcReDiBlE')).toBe('INcReDiBlE');
+});
