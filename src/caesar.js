b4f40a1c2cd64418cf004a823badb48f3d038dcb
Alessandro Vinci <alessandrovinciabc@gmail.com>
2021-04-25 10:32:57 +0200

    Add caesar w/ tests

--- src/caesar.js
+++ src/caesar.js
@@ -1,3 +1,27 @@
-function caesar(string, shift) {}
+function caesar(string, shift) {
+  let alphabet, stringAsArray, shifted;
+
+  alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  stringAsArray = string.split('');
+
+  shifted = stringAsArray
+    .map((letter) => {
+      let isUpperCase = letter.match(/[A-Z]/);
+      if (letter.match(/[^a-z]/i)) return letter;
+
+      let normalized = letter.toLowerCase();
+      let convertedToNumber = alphabet.findIndex((el) => el === normalized);
+      let shiftedNumber = (convertedToNumber + shift) % 26;
+
+      let reconverted = alphabet[shiftedNumber];
+      if (isUpperCase) reconverted = reconverted.toUpperCase();
+
+      return reconverted;
+    })
+    .join('');
+
+  return shifted;
+}
 
 export default caesar;
--- src/caesar.test.js
+++ src/caesar.test.js
@@ -3,3 +3,19 @@ import caesar from './caesar.js';
 test('with 0 shift it returns original string', () => {
   expect(caesar('banana', 0)).toBe('banana');
 });
+
+test('works with negative shifts', () => {
+  expect(caesar('cbobob', -1)).toBe('banana');
+});
+
+test('wraps around after z', () => {
+  expect(caesar('zombie', 1)).toBe('apncjf');
+});
+
+test("doesn't break with punctuation", () => {
+  expect(caesar('a.b', 1)).toBe('b.c');
+});
+
+test('keeps the case', () => {
+  expect(caesar('Banana', 1)).toBe('Cbobob');
+});
